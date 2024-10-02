9c0e7567df3b54c13d182c3eb87750b1230c8e88
KhaledAMARI <kha3d.amari@gmail.com>
2024-10-02 14:56:51 +0100

    Fix paginations

--- src/app/user-table/user-table.component.ts
+++ src/app/user-table/user-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, OnInit, signal } from '@angular/core';
+import { Component, computed, DoCheck, effect, OnInit, signal } from '@angular/core';
 import { ApiService } from '../api.service';
 import { CommonModule } from '@angular/common';
 
@@ -9,48 +9,57 @@ import { CommonModule } from '@angular/common';
   templateUrl: './user-table.component.html',
   styleUrl: './user-table.component.scss'
 })
-export class UserTableComponent implements OnInit {
-  users: any[] = [];
-  currentPage: number = 1; // Current page number
+export class UserTableComponent implements OnInit, DoCheck {
+  users = signal<any[]>([]);
+  currentPage = signal<number>(1); // Current page number
   itemsOptions: number[] = [10, 15, 20]; // Options for items per page
   itemsPerPage = signal<number>(10); // Default items per page
   sortColumn: string = ''; // Column to sort by
-  sortDirection: 'asc' | 'desc' = 'asc'; // Sort direction
+  sortDirection = signal<'asc' | 'desc'> ('asc'); // Sort direction
+  searchTerm = signal<string>("");
 
   constructor(private apiService: ApiService) {}
 
   async ngOnInit() {
     await this.fetchData();
-  console.log("🚀 ~ UserTableComponent ~ itemsPerPage:", this.itemsPerPage())
+  }
 
+  ngDoCheck(): void {
+      console.log('changes happened!');
   }
 
   fetchData() {
     this.apiService.getData().subscribe(data => {
-      this.users = data;
+      this.users.set(data);
     });
   }
 
   deleteUser(user: any) {
-    this.users = this.users.filter((u: any) => u.id !== user.id);
+    this.users.set(this.users().filter((u: any) => u.id !== user.id));
   };
 
-  // Get the users for the current page
+  handleGlobalSearch(event: Event) {
+    event.stopPropagation();
+    const inputElement = event.target as HTMLInputElement;
+    this.searchTerm.update(value => String(inputElement.value));
+  }
+
+  // Get the users() for the current page
   paginatedUsers = computed(() => {
-    const startIndex = computed(() => (this.currentPage - 1) * this.itemsPerPage());
+    const startIndex = computed(() => (this.currentPage() - 1) * this.itemsPerPage());
     const endIndex = computed(() => startIndex() + this.itemsPerPage());
-    return this.users.slice(startIndex(), endIndex());
+    return this.users().slice(startIndex(), endIndex());
   });
 
-  // // Sort users based on the selected column and direction
-  // sortUsers(users: any[]) {
-  //   if (!this.sortColumn) return users; // No sorting if no column is selected
-  //   return users.sort((a, b) => {
+  // Sort users() based on the selected column and direction
+  // sortUsers(users(): any[]) {
+  //   if (!this.sortColumn) return users(); // No sorting if no column is selected
+  //   return users().sort((a, b) => {
   //     const aValue = a[this.sortColumn];
   //     const bValue = b[this.sortColumn];
 
-  //     if (aValue < bValue) return this.sortDirection === 'asc' ? -1 : 1;
-  //     if (aValue > bValue) return this.sortDirection === 'asc' ? 1 : -1;
+  //     if (aValue < bValue) return this.sortDirection() === 'asc' ? -1 : 1;
+  //     if (aValue > bValue) return this.sortDirection() === 'asc' ? 1 : -1;
   //     return 0;
   //   });
   // }
@@ -58,29 +67,29 @@ export class UserTableComponent implements OnInit {
   // Change page
   changePage(page: number) {
     if (page < 1 || page > this.totalPages()) return; // Prevent out-of-bounds
-    this.currentPage = page;
+    this.currentPage.set(page);
   }
 
   // Get total pages
-  totalPages = computed(() => Math.ceil(this.users.length / this.itemsPerPage()));
+  totalPages = computed(() => Math.ceil(this.users().length / this.itemsPerPage()));
 
   // Update items per page
   updateItemsPerPage(event: Event) {
     console.log("🚀 ~ UserTableComponent ~ updateItemsPerPage ~ event:", event)
     const selectElement = event.target as HTMLSelectElement;
     this.itemsPerPage.set(Number(selectElement.value));
-    this.currentPage = 1; // Reset to the first page when items per page changes
+    this.currentPage.set(1); // Reset to the first page when items per page changes
   }
 
   // Sort by column
   sortBy(column: string) {
     if (this.sortColumn === column) {
       // If the same column is clicked, toggle the sort direction
-      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+      this.sortDirection.set(this.sortDirection() === 'asc' ? 'desc' : 'asc');
     } else {
       // Set the new column and default to ascending
       this.sortColumn = column;
-      this.sortDirection = 'asc';
+      this.sortDirection.set('asc');
     }
   }
 }
